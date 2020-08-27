b36e219ed37d6699b3d1769a16df6f372bda0aed
sunako110 <wsha@student.unimelb.edu.au>
2020-08-27 21:24:30 +1000

    day 35: more intuitive way

--- day35.py
+++ day35.py
@@ -19,33 +19,59 @@ class Node(object):
     return answer
 
 def merge(lists):
-    for i in range(1,len(lists)):
-        while True:
-            a = lists[0]
-            b = lists[i]
-            if not b:
-                break
-            if a.val >= b.val:
-                lists[i] = b.next
-                b.next = a
-                lists[0] = b
-            else:
-                while a.next:
-                    if a.next.val >=b.val:
-                        lists[i] = b.next
-                        b.next = a.next
-                        a.next = b
-                        break
-
-                    a = a.next
-
-                    if not a.next:
-                        lists[i] = b.next
-                        b.next = None
-                        a.next = b
-                        a.next.next = None
-                        break
+    last = len(lists) -1
+    while last != 0:
+        i = 0
+        j = last
+        while i < j:
+            lists[i] = merge_two(lists[i], lists[j])
+            i += 1
+            j -= 1
+            if i >= j:
+                last = j
     return lists[0]
+    # for i in range(1,len(lists)):
+    #     while True:
+    #         a = lists[0]
+    #         b = lists[i]
+    #         if not b:
+    #             break
+    #         if a.val >= b.val:
+    #             lists[i] = b.next
+    #             b.next = a
+    #             lists[0] = b
+    #         else:
+    #             while a.next:
+    #                 if a.next.val >=b.val:
+    #                     lists[i] = b.next
+    #                     b.next = a.next
+    #                     a.next = b
+    #                     break
+    #
+    #                 a = a.next
+    #
+    #                 if not a.next:
+    #                     lists[i] = b.next
+    #                     b.next = None
+    #                     a.next = b
+    #                     a.next.next = None
+    #                     break
+    # return lists[0]
+
+def merge_two(a,b):
+
+    if not a:
+        return b
+    if not b:
+        return a
+    if a.val < b.val:
+        result = a
+        result.next = merge_two(a.next,b)
+    else:
+        result = b
+        result.next = merge_two(a,b.next)
+
+    return result
 
 
 a = Node(1, Node(3, Node(5)))
