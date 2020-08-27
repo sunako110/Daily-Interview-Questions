f9d66f5ee6d88fe69ab5be8254eac730bbd89a57
sunako110 <wsha@student.unimelb.edu.au>
2020-08-28 00:15:24 +1000

    day 38

---
+++ day38.py
@@ -0,0 +1,25 @@
+# 13 - Apr - 2020
+
+# First Missing Positive Integer
+
+# You are given an array of integers. Return the smallest positive integer that is not present in the array.
+# The array may contain duplicate entries.
+#
+# For example, the input [3, 4, -1, 1] should return 2 because it is the smallest positive integer
+# that doesn't exist in the array.
+#
+# Your solution should run in linear time and use constant space.
+
+
+def first_missing_positive(nums):
+    # Fill this in.
+    i = 1
+    while True:
+        if i in nums:
+            i += 1
+        else:
+            return i
+
+
+print(first_missing_positive([3, 4, -1, 1]))
+# 2
